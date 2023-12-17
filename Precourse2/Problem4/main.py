1f6ccc57dd477ce423933c6b84e847d1d2e20cea
Manh Cao <manh.cao@moreh.com.vn>
2023-12-17 20:06:53 +0700

    draft complete problem 4

--- Precourse2/Problem4/main.py
+++ Precourse2/Problem4/main.py
@@ -1,10 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import cv2
 import pickle
+
 
 def load_image(file_path):
     image = np.load(file_path)
     return image
+
 
 def apply_convolution(images, kernel, stride=1, padding=0):
     '''
@@ -20,14 +23,30 @@ def apply_convolution(images, kernel, stride=1, padding=0):
     '''
     ### TODO: fill in here ###
 
+    N, image_H, image_W = images.shape
+    kernel_H, kernel_W = kernel.shape
 
+    image_pad_H = (stride * (image_H - 1) + kernel_H - image_H) // 2
+    image_pad_W = (stride * (image_W - 1) + kernel_W - image_W) // 2
 
+    output_H = (image_H + 2 * image_pad_H - kernel_H) // stride + 1
+    output_W = (image_W + 2 * image_pad_W - kernel_W) // stride + 1
+
+    output_images = np.zeros((N, output_H, output_W))
+
+    for index, image in enumerate(images):
+        padded_image = np.pad(image, ((image_pad_H, image_pad_H), (image_pad_W, image_pad_W)), mode='constant', constant_values=0)
+        output_image = np.zeros((output_H, output_W))
+        for i in range(0, output_H * stride, stride):
+            for j in range(0, output_W * stride, stride):
+                output_image[i // stride, j // stride] = np.mean(np.multiply(padded_image[i:i + kernel_H, j:j + kernel_W], kernel))
+
+        output_images[index] = output_image
 
     return output_images
 
 
 if __name__ == "__main__":
-
     input_file_path = "input_image.npy"
 
     input_images = load_image(input_file_path)
@@ -41,13 +60,32 @@ if __name__ == "__main__":
     result_x = apply_convolution(input_images, sobel_x, stride=1, padding=1)
     result_y = apply_convolution(input_images, sobel_y, stride=1, padding=1)
 
-    # Combine the results
-    result = np.sqrt(result_x**2 + result_y**2)
-    
+    result = np.sqrt(result_x ** 2 + result_y ** 2)
+
+    plt.figure(figsize=(10, 8))
+
+    plt.subplot(2, 2, 1)
+    plt.imshow(result[3], cmap='gray')
+    plt.title('Image 1')
+
+    plt.subplot(2, 2, 2)
+    plt.imshow(result[4], cmap='gray')
+    plt.title('Image 2')
+
+    plt.subplot(2, 2, 3)
+    plt.imshow(result[5], cmap='gray')
+    plt.title('Image 3')
+
+    plt.subplot(2, 2, 4)
+    plt.imshow(result[6], cmap='gray')
+    plt.title('Image 4')
+
+    plt.tight_layout()
+
+    plt.show()
+
     '''
     =================================================================================================
     Save and submit a portion of the processed 32 images. 
     You are free to choose any number of images (recommend 4~8)."
     '''
-
-
